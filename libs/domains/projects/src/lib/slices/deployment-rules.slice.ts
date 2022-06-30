10dfc839fc36b0d40dcdde36a076cb28f8ceb89a
Wiliam Traoré <hi@wil-liam.com>
2022-06-30 11:13:16 +0200

    feat(deployment-rules): change the UI of deployment rules list

--- libs/domains/projects/src/lib/slices/deployment-rules.slice.ts
+++ libs/domains/projects/src/lib/slices/deployment-rules.slice.ts
@@ -148,7 +148,7 @@ export const deploymentRulesSlice = createSlice({
       })
       // update order
       .addCase(updateDeploymentRuleOrder.pending, (state: DeploymentRuleState) => {
-        state.loadingStatus = 'loading'
+        //Don't update the loading
       })
       .addCase(updateDeploymentRuleOrder.fulfilled, (state: DeploymentRuleState, action) => {
         state.joinProjectDeploymentRules = {}
